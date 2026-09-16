e3ca8e837b5befd250a6908379768ee732e22276
Björn Gustin <gustin.bjorn@hotmail.com>
2026-09-17 00:07:34 +0200

    Added Week 2 assignments 2.2 and 2.3

---
+++ Week-2/Opdracht 2.2 Boolean logic en selection statements/Program.cs
@@ -0,0 +1,57 @@
+﻿bool hasDriverLicence = true;
+bool hasCar = true;
+
+int age = 26;
+int minimumAge = 18;
+
+bool isAdult = (age >= minimumAge);
+
+bool canDriveOwnCar = isAdult && hasDriverLicence && hasCar;
+bool mayHaveOwnCar = hasDriverLicence || hasCar;
+
+if (canDriveOwnCar)
+{
+    Console.WriteLine("Deze persoon mag zelfstandig in een auto rijden.");
+}
+else
+{
+    Console.WriteLine("Deze persoon mag niet zelfstandig in een auto rijden.");
+}
+
+if (mayHaveOwnCar)
+{
+    Console.WriteLine("Deze persoon mag een eigen auto bezitten.");
+}
+else
+{
+    Console.WriteLine("Deze persoon heeft waarschijnlijk geen auto.");
+}
+
+bool cannotDriveOwnCar = !canDriveOwnCar;
+Console.WriteLine(cannotDriveOwnCar);
+
+if (!hasCar)
+{
+    Console.WriteLine("Deze persoon heeft geen eigen auto.");
+}
+else
+{
+    Console.WriteLine("Deze persoon heeft wel een eigen auto.");
+}
+
+if (canDriveOwnCar)
+{
+    Console.WriteLine("Deze persoon mag zelfstandig rijden in zijn eigen auto.");
+}
+else if (hasDriverLicence && !hasCar)
+{
+    Console.WriteLine("Deze persoon heeft een rijbewijs, maar geen eigen auto.");
+}
+else if (!hasDriverLicence && hasCar)
+{
+    Console.WriteLine("Deze persoon heeft een auto, maar mag er niet in rijden.");
+}
+else
+{
+    Console.WriteLine("Deze persoon heeft geen rijbewijs en geen auto.");
+}
---
+++ Week-2/Opdracht 2.3 Programmeer Raad het getal/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("Welkom bij Raad Het Getal!");
+
+var randomNumberGenerator = new Random();
+var secretNumber = randomNumberGenerator.Next(1, 101);
+
+var isPlaying = true;
+
+while (isPlaying)
+{
+    Console.Write("Voer een getal in tussen de 1 en 100: ");
+
+    var inputRaw = Console.ReadLine();
+
+    int inputParsed;
+    try
+    {
+        inputParsed = int.Parse(inputRaw);
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Ongeldige invoer, probeer opnieuw.");
+        continue;
+    }
+
+    if (secretNumber > inputParsed)
+    {
+        Console.WriteLine("Hoger.");
+    }
+    else if (secretNumber < inputParsed)
+    {
+        Console.WriteLine("Lager.");
+    }
+    else
+    {
+        Console.WriteLine("Hebbes!");
+        isPlaying = false;
+    }
+}
+
+Console.WriteLine("Einde.");
