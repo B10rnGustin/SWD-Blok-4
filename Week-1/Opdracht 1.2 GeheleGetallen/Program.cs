6b04167bb9197820c4290f99f8fa12053e1496cf
Björn Gustin <gustin.bjorn@hotmail.com>
2026-09-22 13:11:46 +0200

    Added week 1 assignments

---
+++ Week-1/Opdracht 1.2 GeheleGetallen/Program.cs
@@ -0,0 +1,67 @@
+﻿/*byte myFirstByte = 200;
+int age = 35;
+long inhabitants = 7000000000;
+
+Console.WriteLine(myFirstByte);
+Console.WriteLine(age);
+Console.WriteLine(inhabitants);
+
+Console.ReadKey();*/
+
+/* int numberOne = -10;
+uint numberTwo = -10;
+
+Console.WriteLine(numberOne);
+Console.WriteLine(numberTwo);
+
+Console.ReadKey();
+
+error CS0031: Constant value '-10' cannot be converted to a 'uint'
+Foutmelding vermeld dat het een positief getal moet zijn.*/
+
+/*byte byteMinValue = byte.MinValue;
+byte byteMaxValue = byte.MaxValue;
+
+int intMinValue = int.MinValue;
+int intMaxValue = int.MaxValue;
+
+uint uintMinValue = uint.MinValue;
+uint uintMaxValue = uint.MaxValue;
+
+long longMinValue = long.MinValue;
+long longMaxValue = long.MaxValue;
+
+Console.WriteLine($"byte: {byteMinValue,22} {byteMaxValue,22}");
+Console.WriteLine($"int:  {intMinValue,22} {intMaxValue,22}");
+Console.WriteLine($"uint: {uintMinValue,22} {uintMaxValue,22}");
+Console.WriteLine($"long: {longMinValue,22} {longMaxValue,22}");
+
+Het verschil tussen een "int" en "uint"variabel is dat een int gehele negatieve en positieve getallen kan bevatten
+en uint kan alleen 0 en positieve gehele getallen kan bevatten
+
+Een signed integer betekent een positief of negatief getal met een teken. Dit kan dus een + of - zijn.
+
+Een unsigned integer betekent een geheel getal zonder een negatief teken. Het getal kan dus 0 of positief zijn.*/
+
+/*int firstNumber = 20;
+int secondNumber = 6;
+
+Console.WriteLine($"Addition: {firstNumber + secondNumber}");
+Console.WriteLine($"Subtraction: {firstNumber - secondNumber}");
+Console.WriteLine($"Multiplication: {firstNumber * secondNumber}");
+Console.WriteLine($"Division: {firstNumber / secondNumber}");
+Console.WriteLine($"Remainder: {firstNumber % secondNumber}");
+
+Console.ReadKey();*/
+
+/*int myCounter = 0;
+
+Console.WriteLine($"Before increment: {myCounter}");
+myCounter++;
+Console.WriteLine($"After increment: {myCounter}");
+
+Console.WriteLine($"Before decrement: {myCounter}");
+myCounter--;
+Console.WriteLine($"After decrement: {myCounter}");
+
+Console.ReadKey();*/
---
+++ Week-1/Opdracht 1.3 DecimaleGetallen/Program.cs
@@ -0,0 +1,52 @@
+﻿/*float myFloat = 0.1f;
+double myDouble = 0.1;
+decimal myDecimal = 0.1m;
+
+
+Console.WriteLine($"Float:   {myFloat,10}");
+Console.WriteLine($"Double:  {myDouble,10}");
+Console.WriteLine($"Decimal: {myDecimal,10}");*/
+
+
+/*double myDouble = 0;
+decimal myDecimal = 0;
+
+for (int i = 0; i < 10; i++)
+{
+    myDouble += 0.1;
+    myDecimal += 0.1m;
+}
+
+Console.WriteLine($"Double:  {myDouble,20}");
+Console.WriteLine($"Decimal: {myDecimal,20}");
+
+Console.ReadKey();*/
+
+/*Console.WriteLine("Bytes float: " + sizeof(float));
+Console.WriteLine("Bytes double: " + sizeof(double));
+Console.WriteLine("Bytes decimal: " + sizeof(decimal));
+
+Dit komt doordat decimal een hoge precisie heeft voor decimale getallen en een groot bereik heeft.
+Console.ReadKey();*/
+
+/*float myFloatValue = 5.5f;
+double myDoubleValue = myFloatValue;
+
+Console.WriteLine(myFloatValue);
+Console.WriteLine(myDoubleValue);
+
+double myBigDouble = 9.99;
+float mySmallFloat = (float)myBigDouble;
+
+Console.WriteLine(myBigDouble);
+Console.WriteLine(mySmallFloat);
+
+Console.ReadKey();
+
+Bij float naar double hoef je niet te casten, omdat double meer precisie heeft dan float. 
+Bij double naar float moet je wel casten, omdat float minder precisie heeft en er daardoor informatie verloren kan gaan.*/
+
+/*decimal priceDecimal = 19.999m;
+Console.WriteLine("Before: " + priceDecimal);
+decimal roundedDecimal = Math.Round(priceDecimal, 2);
+Console.WriteLine("Rounded price: " +  roundedDecimal);*/
